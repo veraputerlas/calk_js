ed9af66c88f637c9b938bf6c2064bac30dbbf42c
Vera <veraputerls@gmail.com>
2023-03-07 14:43:59 +0600

    add +/-

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const tablo = document.querySelector('.tablo')
 const buttons = document.querySelector('.buttons')
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]
-const controlers = ["+/-", "%", "/", "*", "-", "+", ",", "="]
+const controlers = ["%", "/", "*", "-", "+", ",", "="]
 const res = ["AC"]
+const minus = ["+/-"]
 
 let num1 = 0
 let num2 = 0
@@ -17,25 +18,29 @@ buttons.addEventListener('click', (event) => {
             num1 = +tablo.value;
         }     
     if (numbers.includes(contex) && operator != '') {
-        if (tablo.value == 0) tablo.value = contex
-        else tablo.value += contex
-        num2 = +tablo.value
+        if (tablo.value == 0) tablo.value = contex;
+        else tablo.value += contex;
+        num2 = +tablo.value;
     }
     if (controlers.includes(contex)) {
-        operator = contex
-        tablo.value = 0
+        operator = contex;
+        tablo.value = 0;
     }
     if (num1 != 0 && num2 != 0 && operator != '') {
         tablo.value = calc(num1, num2, operator)
-        num1 = +tablo.value
-        num2 = 0
+        num1 = +tablo.value;
+        num2 = 0;
     }
     if (res.includes(contex)) {
         num1 = 0;
         num2 = 0;
         operator = 0;
-        tablo.value = 0;
-        
+        tablo.value = 0; 
+    }
+    
+    if (minus.includes(contex)) {
+        num1 = (-num1);
+        tablo.value = num1;
     }
 
     console.log(`num1: ${num1}`)
@@ -53,6 +58,8 @@ function calc(number1, number2, operator) {
             return +number1 * +number2
         case "/":
             return +number1 / +number2
+        case "+/-":
+            return num1
 
     }
 }
