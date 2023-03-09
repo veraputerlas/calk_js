0919511e8037cacc8c6c654a528a3258cfa16d28
Vera <veraputerls@gmail.com>
2023-03-09 23:53:16 +0600

    second

--- src/index.js
+++ src/index.js
@@ -1,67 +1,80 @@
-const tablo = document.querySelector('.tablo')
-const buttons = document.querySelector('.buttons')
+const tablo = document.querySelector('.tablo');
+const buttons = document.querySelector('.buttons');
 
-const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ","]
-const controlers = ["%", "/", "*", "-", "+", ",", "="]
-const res = ["AC"]
-const minus = ["+/-"]
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','];
+const controlers = ['%', '/', '*', '-', '+'];
+const res = ['AC'];
+const minus = ['+/-'];
 
-let num1 = 0
-let num2 = 0
-let operator = ''
+let buffer = 0;
+let operator = '';
 
 buttons.addEventListener('click', (event) => {
-    let contex = event.target.innerText
-    if (numbers.includes(contex) && operator == '') {
-        if (tablo.value == 0) tablo.value = contex;
-        else tablo.value += contex;
-            num1 = +tablo.value;
-        }     
-    if (numbers.includes(contex) && operator != '') {
-        if (tablo.value == 0) tablo.value = contex;
-        else tablo.value += contex;
-        num2 = +tablo.value;
-    }
-    if (controlers.includes(contex)) {
-        operator = contex;
-        tablo.value = 0;
-    }
-    if (num1 != 0 && num2 != 0 && operator != '') {
-        tablo.value = calc(num1, num2, operator)
-        num1 = +tablo.value;
-        num2 = 0;
-    }
-    if (res.includes(contex)) {
-        num1 = 0;
-        num2 = 0;
-        operator = 0;
-        tablo.value = 0; 
-    }
-    
-    if (minus.includes(contex)) {
-        num1 = (-num1);
-        tablo.value = num1;
+  let contex = event.target.innerText;
+
+  if (controlers.includes(contex)) {
+    buffer = 0;
+    operator = contex;
+  }
+
+  if (res.includes(contex)) {
+    buffer = 0;
+    operator = '';
+    tablo.value = 0;
+  }
+
+  if (minus.includes(contex)) {
+    tablo.value = -tablo.value;
+  }
+
+  if (numbers.includes(contex)) {
+    if (contex === ',' && tablo.value.includes(',')) {
+      return;
     }
 
-    console.log(`num1: ${num1}`)
-    console.log(`num2: ${num2}`)
-    console.log(`operator: ${operator}`)
+    if (operator) {
+      if (buffer == 0) {
+        buffer = tablo.value;
+        tablo.value = contex;
+      } else if (tablo.value == 0) {
+        tablo.value = contex;
+      } else {
+        tablo.value += contex;
+      }
+    } else {
+      if (buffer == 0) {
+        buffer = contex;
+      } else {
+        buffer += contex;
+      }
+      tablo.value = buffer;
+    }
+  }
+
+  if (operator && contex == '=') {
+    tablo.value = convertSeparator(calc().toFixed(2), true).replace(/\,0+$/, '');
+    operator = '';
+  }
 })
 
-function calc(number1, number2, operator) {
-    switch (operator) {
-        case "+":
-            return +number1 + +number2
-        case "-":
-            return +number1 - +number2
-        case "*":
-            return +number1 * +number2
-        case "/":
-            return +number1 / +number2
-        case "%":
-            return +number1 % +number2
-        case "+/-":
-            return num1
+function calc() {
+  let a = convertSeparator(buffer);
+  let b = convertSeparator(tablo.value);
 
-    }
+  switch (operator) {
+    case '+':
+      return +a + +b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+    case '%':
+      return a % b;
+  }
+}
+
+function convertSeparator(value, reverse) {
+  return reverse ? value.toString().replace('.', ',') : value.toString().replace(',', '.');
 }
